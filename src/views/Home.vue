0184eff87141e9cc0cb8c1dd4fcc2d9a79e89668
Scenaristeur <scenaristeur@gmail.com>
2022-01-03 18:18:15 +0100

    dep update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <P5Trois />
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
     <P5Deux />
     <P5First />
   </div>
@@ -15,6 +16,7 @@ export default {
   components: {
     'P5First': () => import('@/components/test/P5First'),
     'P5Deux': () => import('@/components/test/P5Deux'),
+    'P5Trois': () => import('@/components/test/P5Trois'),
   },
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,5 @@ const publicPath = process.env.NODE_ENV === 'production' ? '/p5-vue-solid/' : '/
 module.exports = {
   // options...
   publicPath: publicPath,
+  lintOnSave: false
 }
